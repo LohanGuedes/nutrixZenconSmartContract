011d93b0dc2677ce4ce6a38c5d4105f97567f4e5
Lguedes <lohan.guedes@nutrix.tech>
2023-09-20 01:48:11 -0300

    MVP

--- contracts/nutrixContract.sol
+++ contracts/nutrixContract.sol
@@ -46,55 +46,61 @@ contract NutrixMedSec is ERC721URIStorage, AccessControl {
   struct UserData {
     string[] _patientRecords;
     mapping(address => MedData) _meds;
+    bool isValue;
   }
 
   struct MedData {
     string _medId;
+    bool isValue;
   }
 
   mapping(address => UserData) internal usersData;
   mapping(address => MedData) internal medsData;
 
   constructor() ERC721("NutrixMedSec", "NMS") {
-    _grantRole(ADMIN, msg.sender); // Give admin role to Nutrix wallet
+    _grantRole(ADMIN, msg.sender);
   }
 
   function createPatient(address _userWallet, string memory _initialPatientRecord) public onlyRole(ADMIN) {
-    require(usersData[_userWallet]._patientRecords.length == 0, "User already exists");
+    require(!usersData[_userWallet].isValue, "User already exists");
     UserData storage userData = usersData[_userWallet];
+    userData.isValue = true;
     userData._patientRecords.push(_initialPatientRecord);
     emit CreateUser(_userWallet, _initialPatientRecord);
   }
 
   function createMed(address _medWallet, string memory _medId) public onlyRole(ADMIN) {
-    require(bytes(medsData[_medWallet]._medId).length > 0, "Medic already exists");
+    require(!medsData[_medWallet].isValue, "User already exists");
     MedData storage medData = medsData[_medWallet];
     medData._medId = _medId;
+    medData.isValue = true;
     emit CreateMed(_medWallet, _medId);
   }
 
   function addNewPatientRecord(address userWallet, string memory newPatientRecord) public onlyRole(ADMIN) {
-    require(usersData[userWallet]._patientRecords.length > 0, "Given Wallet does not have data mapped to it");
+    require(usersData[userWallet].isValue, "Given Wallet does not have data mapped to it");
     usersData[userWallet]._patientRecords.push(newPatientRecord);
     emit addUserRecordByWallet(userWallet);
   }
 
-  function grantMedAccessToPatient(address user, address med, string memory medId) public onlyRole(ADMIN) {
-    require(usersData[user]._patientRecords.length > 0, "Given Wallet is not a recognized user");
-    usersData[user]._meds[med]._medId = medId; // Assign med access to user
+  function grantMedAccessToPatient(address user, address med) public onlyRole(ADMIN) {
+    require(usersData[user].isValue, "Given Wallet is not a recognized user");
+    require(medsData[med].isValue, "Given Wallet is not a recognized user");
+
+    
+    usersData[user]._meds[med] = medsData[med];
   }
 
-  /* Gives memory overflow for some reason */
-  function getPatientLatestRecord(address user, address med) public onlyRole(ADMIN) returns (string memory) {
-    require(usersData[user]._patientRecords.length > 0, "Given Wallet is not a recognized user");
-    require(bytes(medsData[med]._medId).length > 0, "Given Wallet is not a recognized medic");
-    require(bytes(usersData[user]._meds[med]._medId).length > 0, "Given medic wallet, has no access. to this user data.");
-    require(usersData[user]._patientRecords.length > 0, "There's no records");
+  modifier medHasAccess(address user, address med) {
+    require(usersData[user]._meds[med].isValue, "Given wallet has no access to user data");
+    _;
+  }
 
+  function getPatientLatestRecord(address user, address med) public onlyRole(ADMIN) medHasAccess(user, med) returns (string memory) {
     uint256 latest = usersData[user]._patientRecords.length - 1;
     string memory latestRecord = usersData[user]._patientRecords[latest];
-    emit DebugLatestRecord(user, latestRecord); // TODO
-    emit med_readLatestRecord(user, med); // MVP, Might be a privacy problem
+    emit DebugLatestRecord(user, latestRecord);
+    emit med_readLatestRecord(user, med);
     return latestRecord;
   }
 }
